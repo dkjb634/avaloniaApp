b1a8b048cf3c2c8a231c09f13dff6534123d3aa3
Dmitrii.Kazantsev <dmitry.kazantsev@jetbrains.com>
2023-02-15 16:44:07 +0200

    Initial

---
+++ ava.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaAppTemplate.Namespace;
+
+public partial class ava : Window
+{
+    public ava()
+    {
+        InitializeComponent();
+    }
+}
