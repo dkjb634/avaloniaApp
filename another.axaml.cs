b7cd2fd073e31716f94a8f1779b4c39c6c42655f
Dmitrii.Kazantsev <dmitry.kazantsev@jetbrains.com>
2023-09-04 15:47:40 +0300

    eqw

---
+++ another.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ava;
+
+public partial class another : UserControl
+{
+    public another()
+    {
+        InitializeComponent();
+    }
+}
